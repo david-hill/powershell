83f9064e0058da89c21087dcb628063392215df3
David Hill <dhill@redhat.com>
2020-07-25 20:57:44 -0400

    Do not move folders if artist contains "&" or "/" ...

--- find_move.ps1
+++ find_move.ps1
@@ -4,18 +4,19 @@ $taglib = "d:\tools\taglib-sharp.dll"
 function find_artist {
   $_=$args[0]
   $media = [taglib.file]::create($_.FullName)
-  $fileperformers = $media.tag.performers
-  $filealbumartists = $media.tag.albumartists
+  $tags = $media.GetTag('Id3v2')
+  $fileperformers = $tags.performers
+  $filealbumartists = $tags.albumartists
   if (-not ( $fileperformers -eq $null) -and [string]::IsNullOrEmpty($fileperformers) -eq $False ) {
     return $fileperformers
-  } elseif ( -not ( $filealbumartists -eq $null )  -and [string]::IsNullOrEmpty($filealbumartists) -eq $False ) {
+  } elseif ( -not ( $filealbumartists -eq $null ) -and [string]::IsNullOrEmpty($filealbumartists) -eq $False ) {
     return $filealbumartists
   } else {
     return $False
   }
 }
 
-$basepath="D:\soulseek-downloads\complete\"
+$basepath="D:\soulseek-downloads\complete"
 $a = Get-ChildItem $basepath -recurse | Where-Object {$_.PSIsContainer -eq $True}
 
 $a | Where-Object {$_.GetFiles().Count -ne 0 -and $_.GetDirectories().count -eq 0} | get-childitem | ForEach-Object {
@@ -23,16 +24,22 @@ $a | Where-Object {$_.GetFiles().Count -ne 0 -and $_.GetDirectories().count -eq 
     if (Test-Path -LiteralPath $_.FullName)  {
       $artist=find_artist $_
       if (-not ( $artist -eq $False) ) {
-	$dir=Split-Path -Path $_.FullName -Parent
-        if ( -not ( "$dir" -like "$basepath\$artist\*" ) -and -not ("$dir" -like "$basepath\$artist") ) {
-          write-host "dir" $dir
-          write-host $artist
-          if (-not(Test-Path -LiteralPath "$basepath\$artist") ) {
-            New-Item -Path "$basepath" -Name "$artist" -ItemType "directory"
+        if ( ( -not ( $artist -match "\&") ) -and ( -not ( $artist -match "\/" ) ) ) {
+  	  $dir=Split-Path -Path $_.FullName -Parent
+          if ( -not ( "$dir" -like "$basepath\$artist\*" ) -and -not ("$dir" -like "$basepath\$artist") ) {
+            write-host "dir" $dir
+            write-host "$basepath\$artist"
+            write-host $artist
+            if (-not(Test-Path -LiteralPath "$basepath\$artist") ) {
+              New-Item -Path "$basepath" -Name "$artist" -ItemType "directory"
+            }
+            move-item -literalpath "$dir" -destination "$basepath\$artist" -Force
           }
-          move-item -literalpath "$dir" -destination "$basepath\$artist" -Force
+        } else {
+          write-host "$artist"
         }
       }
     }
   }
 }
+
