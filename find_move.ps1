8d8cdb3f103e56e3a393d40e2077b4481ca31382
David Hill <dhill@redhat.com>
2023-11-21 16:21:11 -0500

    Update find_move.ps1
    Add a progress bar... it's cute ;)

--- find_move.ps1
+++ find_move.ps1
@@ -1,21 +1,22 @@
 $taglib = "d:\tools\taglib-sharp.dll"
-[system.reflection.assembly]::loadfile($taglib)
+[system.reflection.assembly]::loadfile($taglib) | out-null
 
 $a = $null
 $basepath="D:\soulseek-downloads\complete"
 
-function get_list {
-	write-host "`nGetting file list"
-	$a = Get-ChildItem $basepath -recurse | Where-Object {$_.PSIsContainer -eq $True}
-	return $a
-}
-
 function cleanup {
 	$a = $args[0]
-	if (-not $a -eq $null) {
-		write-host "Cleaning up emtpy folders"
+	if ($a -ne $null) {
+		write-host "Cleaning up emtpy folders... " -nonewline
 		$a | Where-Object {$_.GetFiles().Count -eq 0 -and $_.GetDirectories().count -eq 0} | Remove-Item
+		write-host "done.`n" -nonewline
 	}
+}
+function get_list {
+	write-host "`nGetting file list... " -nonewline
+	$a = Get-ChildItem $basepath -recurse | Where-Object {$_.PSIsContainer -eq $True}
+	write-host "done.`n" -nonewline
+	return $a
 }
 
 function find_artist {
@@ -45,15 +46,18 @@ function findAndMove {
 	$total = 0
 	$inc = 0
 	$last = -1
-	if (-not $a -eq $null) {
+	if ($a -ne $null) {
+		write-host "`nCounting objects... " -nonewline
 		$total = ($a | Where-Object {$_.GetFiles().Count -ne 0 -and $_.GetDirectories().count -eq 0} | get-childitem | Measure-Object).Count
+		write-host "done.`n" -nonewline
 		write-host "`nProcessing $total items`n"
 		$a | Where-Object {$_.GetFiles().Count -ne 0 -and $_.GetDirectories().count -eq 0} | get-childitem | ForEach-Object {
 			if ( $_.FullName -like '*mp3' -or $_.FullName -like '*flac' -or $_.FullName -like '*m4a' -or $_.FullName -like '*wav') {
 				$inc = $inc + 1;
 				$progress = [math]::floor($inc / $total * 100)
 				if ($progress -ne $last) {
-					write-host "`rProgress: $progress %" -nonewline
+					#write-host "`rProgress: $progress %" -nonewline
+					Write-Progress -Activity "Search in Progress" -Status "$progress% Complete:" -PercentComplete $progress
 					$last = $progress
 				}
 				if (Test-Path -LiteralPath $_.FullName)  {
